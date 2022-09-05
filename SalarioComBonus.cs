984aa015955059949e3b02f52613531e03a48ad1
Pedro Reinbold <104009313+PedroReinbold@users.noreply.github.com>
2022-09-05 12:18:01 -0300

    Add files via upload

---
+++ SalarioComBonus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            double salary, sell, comissao, discount;
+
+            name = (Console.ReadLine());
+            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            sell = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            discount = sell * 0.85;
+            comissao = sell - discount;
+
+            Console.WriteLine("TOTAL = R$ " + (salary + comissao).ToString("F2"));
+        }
+    }
+}
