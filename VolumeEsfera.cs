779925676ed9aecc0dae9dfdb394fded7c9cd125
Pedro Reinbold <104009313+PedroReinbold@users.noreply.github.com>
2022-09-06 11:41:21 -0300

    Add files via upload

---
+++ VolumeEsfera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vol, pi = 3.14159;
+
+            Console.WriteLine("Digite o valor do raio: ");
+
+            double raio = double.Parse(Console.ReadLine());
+
+            vol = (4 / 3.0) * pi * Math.Pow(raio, 3.0);
+
+            Console.WriteLine("VOLUME = " + (vol).ToString("F3"));
+
+        }
+    }
+}
